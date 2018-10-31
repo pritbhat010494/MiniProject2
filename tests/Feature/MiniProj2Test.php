889f90581e2ce00a28c930fe2643e63dd94ef302
pritam <pb457@njit.edu>
2018-10-31 15:51:05 -0400

    Task:Contact Page Feature test done

--- tests/Feature/MiniProj2Test.php
+++ tests/Feature/MiniProj2Test.php
@@ -30,4 +30,10 @@ class MiniProj2Test extends TestCase
         $response = $this->get('/about');
         $response->assertStatus(200);
     }
+
+    public function testContactPage()
+    {
+        $response = $this->get('/contact');
+        $response->assertStatus(200);
+    }
 }
