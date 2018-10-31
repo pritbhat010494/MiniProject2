6ce9c717fcfef3ef0b510f4dd0657daa621bd9da
pritam <pb457@njit.edu>
2018-10-31 04:10:11 -0400

    Addon: Edited CarFactory & UsersTableSeeder

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -5,7 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(\App\Car::class, function (Faker $faker) {
     return [
         'Year' => $faker->Year('999'),
-        'Model'=> $faker->name(),
+        'Model'=> $faker->randomElement($array = array ('Sedan', 'Camry', 'Fiesta')),
         'Make' => $faker->randomElement($array = array ('Honda', 'Toyota', 'Ford')),
     ];
 });
+
