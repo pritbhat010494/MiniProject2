63c10ef1d31e6043501d0a6dfc095b7762ce30f9
pritam <pb457@njit.edu>
2018-10-31 16:21:25 -0400

    Task:Unit test to insert a user in users table done

---
+++ tests/Unit/InsertTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class InsertTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertUser()
+    {
+        $user = new User();
+        $user->name = "Pritam";
+        $user->email = "bhatkarpritam@gmail.com";
+        $user->password = "abcd";
+        $this->assertTrue($user->save());
+        $this->assertTrue(true);
+
+
+    }
+}
