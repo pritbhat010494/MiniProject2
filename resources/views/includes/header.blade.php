a2b8cc73068a426c4169652c262cea1a491b86fa
pritam <pb457@njit.edu>
2018-10-31 04:43:04 -0400

    Task: Created includes directory under views and addon head,header and footer

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-danger fixed-top">
+        <style>
+            body {background-color: #b3e8ca ;}
+        </style>
+        <a class="navbar-brand" href="/">MiniProject2</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link " href="{{route('about')}}">About</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="{{route('contact')}}">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
