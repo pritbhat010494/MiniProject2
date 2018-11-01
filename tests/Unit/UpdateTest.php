637c160d9c5ec5bbf45950362a046f0940600489
pritam <pb457@njit.edu>
2018-11-01 10:24:32 -0400

    Task:Created unit test & Updated the name of the user to Steve Smith

---
+++ tests/Unit/UpdateTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UpdateTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUpdateUser()
+    {
+        $user = User::find(1);
+        $user->name = "Steve Smith";
+        $this->assertTrue($user->save());
+    }
+}
