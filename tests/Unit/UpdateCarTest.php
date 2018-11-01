17303e668740188282302969ab2be5db755d80a2
pritam <pb457@njit.edu>
2018-11-01 11:59:59 -0400

    Task:Created UpdateCar Test to update the car Year to '2000'

---
+++ tests/Unit/UpdateCarTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class UpdateCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUpdateCar()
+    {
+        $car = Car::find(1);
+        $car->Year = '2000';
+        $this->assertTrue($car->save());
+    }
+}
