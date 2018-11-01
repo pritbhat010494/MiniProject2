2528bc2cbed77b6a7981a29eacf5cbb4341599a0
pritam <pb457@njit.edu>
2018-11-01 11:29:19 -0400

    Task:CountTest to count number of users inserted by seeding

---
+++ tests/Unit/CountTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class CountTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCountTest()
+    {
+        $user = User::all();
+        $recordCount = $user->count();
+        $this->assertLessThanOrEqual(50, $recordCount);
+    }
+}
