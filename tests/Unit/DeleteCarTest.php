b69eca24dfd0a22b20efbdad35781d88e84b581f
pritam <pb457@njit.edu>
2018-11-01 12:08:15 -0400

    Task:Created DeleteCar Test to delete car from database

---
+++ tests/Unit/DeleteCarTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class DeleteCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testDeleteCar()
+    {
+        $car= new Car();
+        $car->Year= '1970';
+        $car->Model = 'Sedan';
+        $car->Make = 'Ford';
+        $car->save();
+        $this->assertTrue($car->delete());
+    }
+}
